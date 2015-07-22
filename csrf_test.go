3a7f9c6737eabcd8cd2a0e46c7c75e04cd710563
Matt Silverlock <matt@eatsleeprepeat.net>
2015-07-22 16:42:46 +0800

    v1.0rc1: goji/csrf.

---
+++ csrf_test.go
@@ -0,0 +1,277 @@
+package csrf
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/zenazn/goji/web"
+)
+
+var testKey = []byte("keep-it-secret-keep-it-safe-----")
+var testHandler = web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {})
+
+// TestProtect is a high-level test to make sure the middleware returns the
+// wrapped handler with a 200 OK status.
+func TestProtect(t *testing.T) {
+	s := web.New()
+	s.Use(Protect(testKey))
+
+	s.Get("/", testHandler)
+
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusOK {
+		t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+
+	if rr.Header().Get("Set-Cookie") == "" {
+		t.Fatalf("cookie not set: got %q", rr.Header().Get("Set-Cookie"))
+	}
+}
+
+// Test that idempotent methods return a 200 OK status and that non-idempotent
+// methods return a 403 Forbidden status when a CSRF cookie is not present.
+func TestMethods(t *testing.T) {
+	s := web.New()
+	s.Use(Protect(testKey))
+
+	s.Handle("/", web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {
+	}))
+
+	// Test idempontent ("safe") methods
+	for _, method := range safeMethods {
+		r, err := http.NewRequest(method, "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		s.ServeHTTP(rr, r)
+
+		if rr.Code != http.StatusOK {
+			t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+				rr.Code, http.StatusOK)
+		}
+
+		if rr.Header().Get("Set-Cookie") == "" {
+			t.Fatalf("cookie not set: got %q", rr.Header().Get("Set-Cookie"))
+		}
+	}
+
+	// Test non-idempotent methods (should return a 403 without a cookie set)
+	nonIdempotent := []string{"POST", "PUT", "DELETE", "PATCH"}
+	for _, method := range nonIdempotent {
+		r, err := http.NewRequest(method, "/", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		s.ServeHTTP(rr, r)
+
+		if rr.Code != http.StatusForbidden {
+			t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+				rr.Code, http.StatusOK)
+		}
+
+		if rr.Header().Get("Set-Cookie") == "" {
+			t.Fatalf("cookie not set: got %q", rr.Header().Get("Set-Cookie"))
+		}
+	}
+
+}
+
+// Tests for failure if the cookie containing the session is removed from the
+// request.
+func TestNoCookie(t *testing.T) {
+
+}
+
+// TestBadCookie tests for failure when a cookie header is modified (malformed).
+func TestBadCookie(t *testing.T) {
+	s := web.New()
+	CSRF := Protect(testKey)
+	s.Use(CSRF)
+
+	var token string
+	s.Handle("/", web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {
+		token = Token(c, r)
+	}))
+
+	// Obtain a CSRF cookie via a GET request.
+	r, err := http.NewRequest("GET", "http://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	// POST the token back in the header.
+	r, err = http.NewRequest("POST", "http://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Replace the cookie prefix
+	badHeader := strings.Replace("_csrfToken=", rr.Header().Get("Set-Cookie"), "_badCookie", -1)
+	r.Header.Set("Cookie", badHeader)
+	r.Header.Set("X-CSRF-Token", token)
+	r.Header.Set("Referer", "http://www.gorillatoolkit.org/")
+
+	rr = httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusForbidden {
+		t.Fatalf("middleware failed to reject a bad cookie: got %v want %v",
+			rr.Code, http.StatusForbidden)
+	}
+
+}
+
+// Responses should set a "Vary: Cookie" header to protect client/proxy caching.
+func TestVaryHeader(t *testing.T) {
+
+	s := web.New()
+	s.Use(Protect(testKey))
+	s.Get("/", testHandler)
+
+	r, err := http.NewRequest("HEAD", "https://www.golang.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusOK {
+		t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+
+	if rr.Header().Get("Vary") != "Cookie" {
+		t.Fatalf("vary header not set: got %q want %q", rr.Header().Get("Vary"), "Cookie")
+	}
+}
+
+// Requests with no Referer header should fail.
+func TestNoReferer(t *testing.T) {
+
+	s := web.New()
+	s.Use(Protect(testKey))
+	s.Handle("/", web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {}))
+
+	r, err := http.NewRequest("POST", "https://golang.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusForbidden {
+		t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+			rr.Code, http.StatusForbidden)
+	}
+}
+
+// TestBadReferer checks that HTTPS requests with a Referer that does not
+// match the request URL correctly fail CSRF validation.
+func TestBadReferer(t *testing.T) {
+
+	s := web.New()
+	CSRF := Protect(testKey)
+	s.Use(CSRF)
+
+	var token string
+	s.Handle("/", web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {
+		token = Token(c, r)
+	}))
+
+	// Obtain a CSRF cookie via a GET request.
+	r, err := http.NewRequest("GET", "https://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	// POST the token back in the header.
+	r, err = http.NewRequest("POST", "https://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	setCookie(rr, r)
+	r.Header.Set("X-CSRF-Token", token)
+
+	// Set a non-matching Referer header.
+	r.Header.Set("Referer", "http://goji.io")
+
+	rr = httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusForbidden {
+		t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+			rr.Code, http.StatusForbidden)
+	}
+}
+
+// Requests with a valid Referer should pass.
+func TestWithReferer(t *testing.T) {
+	s := web.New()
+	CSRF := Protect(testKey)
+	s.Use(CSRF)
+
+	var token string
+	s.Handle("/", web.HandlerFunc(func(c web.C, w http.ResponseWriter, r *http.Request) {
+		token = Token(c, r)
+	}))
+
+	// Obtain a CSRF cookie via a GET request.
+	r, err := http.NewRequest("GET", "http://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	// POST the token back in the header.
+	r, err = http.NewRequest("POST", "http://www.gorillatoolkit.org/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	setCookie(rr, r)
+	r.Header.Set("X-CSRF-Token", token)
+	r.Header.Set("Referer", "http://www.gorillatoolkit.org/")
+
+	rr = httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusOK {
+		t.Fatalf("middleware failed to pass to the next handler: got %v want %v",
+			rr.Code, http.StatusOK)
+	}
+}
+
+// TestFormField tests that a token in the form field takes precedence over a
+// token in the HTTP header.
+func TestFormField(t *testing.T) {
+
+}
+
+func setCookie(rr *httptest.ResponseRecorder, r *http.Request) {
+	r.Header.Set("Cookie", rr.Header().Get("Set-Cookie"))
+}
---
+++ store_test.go
@@ -0,0 +1,100 @@
+package csrf
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/securecookie"
+	"github.com/zenazn/goji/web"
+)
+
+// brokenSaveStore is a CSRF store that cannot, well, save.
+type brokenSaveStore struct {
+	Store
+}
+
+func (bs *brokenSaveStore) Get(*web.C, *http.Request) ([]byte, error) {
+	// Generate an invalid token so we can progress to our Save method
+	return generateRandomBytes(24)
+}
+
+func (bs *brokenSaveStore) Save(realToken []byte, w http.ResponseWriter) error {
+	return errors.New("test error")
+}
+
+func setStore(s Store) func(*csrf) error {
+	return func(cs *csrf) error {
+		cs.store = s
+		return nil
+	}
+}
+
+// Tests for failure if the middleware can't save to the Store.
+func TestStoreCannotSave(t *testing.T) {
+	s := web.New()
+	bs := &brokenSaveStore{}
+	s.Use(Protect(testKey, setStore(bs)))
+	s.Get("/", testHandler)
+
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	s.ServeHTTP(rr, r)
+
+	if rr.Code != http.StatusForbidden {
+		t.Fatalf("broken store did not set an error status: got %v want %v",
+			rr.Code, http.StatusForbidden)
+	}
+
+	if c := rr.Header().Get("Set-Cookie"); c != "" {
+		t.Fatalf("broken store incorrectly set a cookie: got %v want %v",
+			c, "")
+	}
+
+}
+
+// TestCookieDecode tests that an invalid cookie store returns a decoding error.
+func TestCookieDecode(t *testing.T) {
+	r, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var age = 3600
+
+	// Test with a nil hash key
+	sc := securecookie.New(nil, nil)
+	sc.MaxAge(age)
+	store := &CookieStore{cookieName, age, sc}
+
+	// Set a fake cookie value so r.Cookie passes.
+	r.Header.Set("Cookie", fmt.Sprintf("%s=%s", cookieName, "notacookie"))
+
+	_, err = store.Get(&web.C{}, r)
+	if err == nil {
+		t.Fatal("cookiestore did not report an invalid hashkey on decode")
+	}
+}
+
+// TestCookieEncode tests that an invalid cookie store returns an encoding error.
+func TestCookieEncode(t *testing.T) {
+	var age = 3600
+
+	// Test with a nil hash key
+	sc := securecookie.New(nil, nil)
+	sc.MaxAge(age)
+	store := &CookieStore{cookieName, age, sc}
+
+	rr := httptest.NewRecorder()
+
+	err := store.Save(nil, rr)
+	if err == nil {
+		t.Fatal("cookiestore did not report an invalid hashkey on encode")
+	}
+}
